60afe79fb18a9e5a71d594f5685f2d212f984285
Toshal <43895919+Maspie@users.noreply.github.com>
2025-02-26 10:57:34 -0700

    Time: 10 ms (42.71%) | Memory: 12.5 MB (16.53%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -17,7 +17,7 @@ class Solution(object):
                 if board[i][j] == ".":
                     continue
 
-                if board[i][j] in row[i] or board[i][j] in col[j] or board[i][j] in sqa[i//3, j//3]:
+                if board[i][j] in row[i] or board[i][j] in col[j] or board[i][j] in sqa[(i//3, j//3)]:
                     return False
 
                 row[i].add(board[i][j])
