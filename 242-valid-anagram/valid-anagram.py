93428c28b24f2ed4406acdbdc9d63b5afd6e71e1
Toshal <43895919+Maspie@users.noreply.github.com>
2025-02-25 22:13:27 -0700

    Time: 21 ms (59.66%) | Memory: 14.6 MB (5.10%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -6,4 +6,15 @@ class Solution(object):
         :rtype: bool
         """
         # return Counter(s) == Counter(t)
-        return sorted(s) == sorted(t)
+        # return sorted(s) == sorted(t)
+
+        if (len(s) != len(t)):
+            return False
+
+        hashs= {}
+        hasht = {}
+        for i in range(len(s)):
+            hashs[s[i]] = 1 + hashs.get(s[i], 0)
+            hasht[t[i]] = 1 + hasht.get(t[i], 0)
+
+        return hashs == hasht
