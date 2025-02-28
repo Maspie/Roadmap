eccbaefca21810abb300d0ea7e73f41de6d64ac4
Toshal <43895919+Maspie@users.noreply.github.com>
2025-02-28 00:06:08 -0700

    Time: 12 ms (76.58%) | Memory: 12.8 MB (80.69%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,23 +4,30 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        lis =[]
         s = s.lower()
-        for char in s:
-            if char.isalnum():
-                lis.append(char)
-        print(s)    
+   
         l = 0
-        r = len(lis) - 1
+        r = len(s) - 1
         while l < r:
 
-            if lis[l] == lis[r]:
+            while l < r and not s[l].isalnum():
                 l += 1
+
+            while r > l and not s[r].isalnum():
                 r -= 1
+            
+
+
+            if s[l] != s[r]:
+                return False
+        
+
+            l += 1
+            r -= 1
+            
                 
             
-            else:
-                return False
+            
             
 
         return True
