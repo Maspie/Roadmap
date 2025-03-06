dd56b4d4a21bba811341f6e4bdbf98900fb72b96
Toshal <43895919+Maspie@users.noreply.github.com>
2025-03-06 15:17:49 -0700

    Time: 235 ms (27.11%) | Memory: 15.7 MB (26.31%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        if len(t) > len(s):
+            return ""
+        
+        
+
+        count_t = Counter(t)
+        
+        l = 0   
+        need = len(count_t)
+        have = 0
+        min_streak = float("inf")
+        window = {}
+        res = ""
+        for r in range(len(s)):
+            char = s[r]
+            window[char] = 1 + window.get(char, 0)
+
+            if char in count_t and window[char] == count_t[char]:
+                have += 1
+            
+            while have == need:
+
+                if (r-l+1) < min_streak:
+                    min_streak = r-l+1
+                    res = s[l: r+1]
+
+                window[s[l]] -= 1
+
+                if s[l] in count_t and window[s[l]] < count_t[s[l]]:
+                    have -= 1
+                l += 1
+
+        return res
