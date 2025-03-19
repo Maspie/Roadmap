dffedb04c048a89cef3b8289b707f6d17bcf40f5
Toshal <43895919+Maspie@users.noreply.github.com>
2025-03-18 18:07:45 -0700

    Time: 0 ms (100.00%) | Memory: 29.3 MB (5.34%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -2,20 +2,68 @@ import numpy as np
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
-        matrix = np.array(matrix)
-        flatten = matrix.flatten()
-        
-        low  = 0
-        high = len(flatten) - 1
+
+
+
+        bot, top = 0, len(matrix) - 1
+
+        while bot <= top:
+
+            midrow = (bot + top) // 2
+
+            if matrix[midrow][-1] < target:
+
+                bot = midrow + 1
+
+            elif matrix[midrow][0] > target:
+
+                top = midrow - 1
+
+            else:
+                break
+        if not (bot<= top):
+            return False
+            
+        low, high = 0, len(matrix[midrow]) - 1
+
 
         while low <= high:
             mid = (low + high) // 2
 
-            if flatten[mid] == target:
+            if matrix[midrow][mid] == target:
                 return True
-            elif flatten[mid] > target:
+             
+            elif matrix[midrow][mid] > target:
                 high = mid - 1
 
             else:
+
                 low = mid + 1
         return False
+
+
+
+
+
+
+
+
+
+
+        # matrix = np.array(matrix)
+        # flatten = matrix.flatten()
+        
+        # low  = 0
+        # high = len(flatten) - 1
+
+        # while low <= high:
+        #     mid = (low + high) // 2
+
+        #     if flatten[mid] == target:
+        #         return True
+        #     elif flatten[mid] > target:
+        #         high = mid - 1
+
+        #     else:
+        #         low = mid + 1
+        # return False
