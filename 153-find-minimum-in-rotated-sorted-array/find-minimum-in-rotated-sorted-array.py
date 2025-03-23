8d2be453446c1e224779bd1af47bb96fc5d53dd4
Toshal <43895919+Maspie@users.noreply.github.com>
2025-03-22 18:53:13 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (80.84%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,3 +1,18 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        return min(nums)
+        # return min(nums) O(n) solution since it goes through array once, we need log n
+        # log n's 20 times is 1 million for n
+
+        l = 0
+        r = len(nums) - 1
+
+        while l < r:
+
+            mid = (l + r) // 2
+
+            if nums[mid] > nums[r]:
+                l = mid + 1
+ 
+            else:
+                r = mid
+        return nums[l] 
