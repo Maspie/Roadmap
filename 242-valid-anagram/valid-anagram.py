e5ad6145173630bbddbafdfe3353bbac70d7c5b8
Toshal <43895919+Maspie@users.noreply.github.com>
2025-05-04 12:03:03 -0700

    Time: 7 ms (89.30%) | Memory: 18.1 MB (32.75%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,20 +1,4 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        # return Counter(s) == Counter(t)
-        # return sorted(s) == sorted(t)
-
-        if (len(s) != len(t)):
-            return False
-
-        hashs= {}
-        hasht = {}
-        for i in range(len(s)):
-            hashs[s[i]] = 1 + hashs.get(s[i], 0)
-            hasht[t[i]] = 1 + hasht.get(t[i], 0)
-
-        return hashs == hasht
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        return Counter(s) == Counter(t)
