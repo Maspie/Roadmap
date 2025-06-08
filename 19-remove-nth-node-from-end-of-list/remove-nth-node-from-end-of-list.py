2ff7578e31694e754f536a64389d68cfd1404cba
Toshal <43895919+Maspie@users.noreply.github.com>
2025-06-08 13:21:14 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (34.46%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -5,41 +5,56 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        #reversing the list
+        # #reversing the list
 
-        curr = head
-        prev = None
+        # curr = head
+        # prev = None
 
-        while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
-        dummy = ListNode(0, prev)
-        curr = dummy
+        # while curr:
+        #     temp = curr.next
+        #     curr.next = prev
+        #     prev = curr
+        #     curr = temp
+        # dummy = ListNode(0, prev)
+        # curr = dummy
 
-        for _ in range(n-1):
-            curr = curr.next
+        # for _ in range(n-1):
+        #     curr = curr.next
 
-        curr.next = curr.next.next
-        if dummy.next is None:
-            return None
+        # curr.next = curr.next.next
+        # if dummy.next is None:
+        #     return None
 
-        curr = dummy.next
-        prev = None
+        # curr = dummy.next
+        # prev = None
 
-        while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
-
-
+        # while curr:
+        #     temp = curr.next
+        #     curr.next = prev
+        #     prev = curr
+        #     curr = temp
 
 
 
-
-
-        return prev
+        # return prev
             
        
+
+        #slow fast pointer
+        dummy = ListNode(0, head)
+        slow = fast = dummy
+
+        for _ in range(n):
+            fast = fast.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next
+
+        slow.next = slow.next.next if slow.next else None
+
+        return dummy.next
+
+        
+
+        
